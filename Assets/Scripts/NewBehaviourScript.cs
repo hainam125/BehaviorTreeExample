cee85c890a39d49ff5a417f2dab10c966b58f025
GNT-GLOBAL\nam.nh2388 <nam.nh2388@gnt.com.vn>
2018-11-06 15:42:19 +0700

    Parallel Node Example

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -12,26 +12,36 @@ public class NewBehaviourScript : MonoBehaviour {
         var builder = new BehaviourTreeBuilder();
         //if Dist < 0.5 doA else doB
         tree = builder.
-            Selector("Find Target").
-                Sequence("At Target").
-                    Condition("At Target", t => {
-                        Debug.Log("Is At Target");
-                        var dist = Vector3.Distance(transform.position, target.position);
-                        return dist < 0.5f;
-                    }).
-                    Do("Idle", t => {
-                        Debug.Log("Idle");
-                        return BehaviourTreeStatus.Success;
-                    }).
-                End().
-                Do("Go to Target", t =>
-                {
-                    Debug.Log("Go to target");
-                    var dist = Vector3.Distance(transform.position, target.position);
-                    transform.position += (target.position - transform.position).normalized * speed * Time.deltaTime;
+            Parallel("All", 20, 20).
+                Do("RoateX", t => {
+                    transform.GetChild(0).Rotate(new Vector3(60, 0) * Time.deltaTime);
                     return BehaviourTreeStatus.Success;
                 }).
-            End().
+                Do("RoateZ", t => {
+                    transform.GetChild(0).Rotate(new Vector3(0, 0, 60) * Time.deltaTime);
+                    return BehaviourTreeStatus.Success;
+                }).
+                Selector("Find Target").
+                    Sequence("At Target").
+                        Condition("At Target", t => {
+                            Debug.Log("Is At Target");
+                            var dist = Vector3.Distance(transform.position, target.position);
+                            return dist < 0.5f;
+                        }).
+                        Do("Idle", t => {
+                            Debug.Log("Idle");
+                            return BehaviourTreeStatus.Success;
+                        }).
+                    End().
+                    Do("Go to Target", t =>
+                    {
+                        Debug.Log("Go to target");
+                        var dist = Vector3.Distance(transform.position, target.position);
+                        transform.position += (target.position - transform.position).normalized * speed * Time.deltaTime;
+                        return BehaviourTreeStatus.Success;
+                    }).
+                    End().
+                End().
             Build();
 	}
 	
