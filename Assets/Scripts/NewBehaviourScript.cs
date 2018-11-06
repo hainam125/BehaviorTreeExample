454e685cc8a6539a38a1f15b0f7ff32d12051471
GNT-GLOBAL\nam.nh2388 <nam.nh2388@gnt.com.vn>
2018-11-06 14:35:34 +0700

    Init Code

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FluentBehaviourTree;
+
+public class NewBehaviourScript : MonoBehaviour {
+    IBehaviourTreeNode tree;
+
+
+	void Start () {
+        var builder = new BehaviourTreeBuilder();
+        tree = builder.
+            Selector("mySelector").
+                Do("actionx", t => {
+                    Debug.Log("action x");
+                    return BehaviourTreeStatus.Running;
+                }).
+                Sequence("mySequence").
+                    Do("action1", t =>
+                    {
+                        Debug.Log("Action 1");
+                        return BehaviourTreeStatus.Success;
+                    }).
+                    Do("action2", t =>
+                    {
+                        Debug.Log("Action 2");
+                        return BehaviourTreeStatus.Success;
+                    }).
+                End().
+            End().
+            Build();
+	}
+	
+
+	void Update () {
+        tree.Tick(new TimeData(Time.deltaTime));
+	}
+}
